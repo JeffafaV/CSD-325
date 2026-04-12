125e11e477a06b97b3c81473d5f8688e79100d44
Jeff Victorino <jeffyv914@gmail.com>
2026-04-12 16:25:15 -0700

    Add Module 3 Assignments

---
+++ Module-3/chohan_jv.py
@@ -0,0 +1,99 @@
+# Jeff Victorino
+# 4/12/2026
+# Module 3.2 Assignment
+
+"""Cho-Han, by Al Sweigart al@inventwithpython.com
+The traditional Japanese dice game of even-odd.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: short, beginner, game"""
+
+import random, sys
+
+JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN',
+                    4: 'SHI', 5: 'GO', 6: 'ROKU'}
+
+# CHANGED: Added notice about bonus rolls (2 or 7)
+print('''Cho-Han, by Al Sweigart al@inventwithpython.com
+
+In this traditional Japanese dice game, two dice are rolled in a bamboo
+cup by the dealer sitting on the floor. The player must guess if the
+dice total to an even (cho) or odd (han) number.
+
+BONUS: If the dice total equals 2 or 7, you receive a 10 mon bonus!
+''')
+
+purse = 5000
+while True:  # Main game loop.
+    # Place your bet:
+    print('You have', purse, 'mon. How much do you bet? (or QUIT)')
+    while True:
+        pot = input('jv: ')
+        if pot.upper() == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+        elif not pot.isdecimal():
+            print('Please enter a number.')
+        elif int(pot) > purse:
+            print('You do not have enough to make that bet.')
+        else:
+            pot = int(pot)
+            break
+
+    # Roll the dice.
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    rollTotal = dice1 + dice2   # CHANGED: store dice total for bonus logic
+
+    print('The dealer swirls the cup and you hear the rattle of dice.')
+    print('The dealer slams the cup on the floor, still covering the')
+    print('dice and asks for your bet.')
+    print()
+    print('    CHO (even) or HAN (odd)?')
+
+    # Let the player bet cho or han:
+    while True:
+        bet = input('jv: ').upper()
+        if bet != 'CHO' and bet != 'HAN':
+            print('Please enter either "CHO" or "HAN".')
+            continue
+        else:
+            break
+
+    # Reveal the dice results:
+    print('The dealer lifts the cup to reveal:')
+    print('  ', JAPANESE_NUMBERS[dice1], '-', JAPANESE_NUMBERS[dice2])
+    print('    ', dice1, '-', dice2)
+
+    # CHANGED: Bonus payout for rolling a 2 or 7
+    if rollTotal == 2 or rollTotal == 7:
+        print(f'BONUS! You rolled a total of {rollTotal} and earned 10 mon!')
+        purse += 10
+
+    # Determine if the player won:
+    rollIsEven = (rollTotal) % 2 == 0
+    if rollIsEven:
+        correctBet = 'CHO'
+    else:
+        correctBet = 'HAN'
+
+    playerWon = bet == correctBet
+
+    # Display the bet results:
+    if playerWon:
+        print('You won! You take', pot, 'mon.')
+        purse = purse + pot
+
+        # CHANGED: House fee updated from 10% to 12%
+        houseFee = int(pot * 0.12)
+        print('The house collects a', houseFee, 'mon fee.')
+        purse = purse - houseFee
+
+    else:
+        purse = purse - pot
+        print('You lost!')
+
+    # Check if the player has run out of money:
+    if purse == 0:
+        print('You have run out of money!')
+        print('Thanks for playing!')
+        sys.exit()
